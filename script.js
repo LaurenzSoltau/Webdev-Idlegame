ef2ff6cb19841e4d9185cb7766098425f16aeb63
LaurenzSoltau <laurenz.soltau@icloud.com>
2022-06-18 22:21:43 +0200

    Ester Teil von den Grundfunktionen

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+var noodles = 0;
+var priceSmallItalianKid;
+var noodlesPerSecond;
+var amountSmallItalianKid;
+document.getElementById("priceSmallItalianKid")
 
+function Click()
+{
+    noodles = noodles +1;
+    document.getElementById("noodles").innerHTML=noodles;
+}
+
+function buySmallItalianKid()
+{
+    if(noodles<priceSmallItalianKid){
+        return;
+    }
+    noodles = noodles-priceSmallItalianKid;
+    amountSmallItalianKid = amountSmallItalianKid + 1;
+    priceSmallItalianKid = priceSmallItalianKid * 1.07
+    document.getElementById("priceSmallItalianKid").innerHTML=priceSmallItalianKid
+    document.getElementById("noodles").innerHTML=noodles;
+}
